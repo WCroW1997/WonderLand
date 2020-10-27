b9e784850664b4515c3147ac63419ed28ca97a87
WCroW1997 <62266220+WCroW1997@users.noreply.github.com>
2020-10-27 17:33:30 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+   scrollbar: {
+      el: '.swiper-scrollbar',} ,
+   navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+       },
+    autoplay: {
+      delay: 5000,
+      },
+});
